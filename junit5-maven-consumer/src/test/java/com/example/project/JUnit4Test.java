c9027db477d54ca7a78069eaa9bc9edd819f1235
Marc Philipp <mail@marcphilipp.de>
2015-12-15 17:42:26 +0100

    Add JUnit 4 test to showcase Launcher/Engine API

---
+++ junit5-maven-consumer/src/test/java/com/example/project/JUnit4Test.java
@@ -0,0 +1,14 @@
+
+package com.example.project;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JUnit4Test {
+
+	@Test
+	public void test() {
+		assertEquals(3, 1 + 2);
+	}
+}
