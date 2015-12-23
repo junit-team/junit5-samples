fbb33003defc05f41f2800d48374aa60c0fd3499
Johannes Link <business@johanneslink.net>
2015-12-23 20:15:20 +0100

    gradle-consumer now excludes a test by tag.
    ------------------------------------------------------------------------
    On behalf of the community, the JUnit Lambda Team thanks
    Gradleware (https://gradle.org) for supporting the
    JUnit crowdfunding campaign!
    ------------------------------------------------------------------------

--- junit5-gradle-consumer/src/test/java/com/example/project/SecondTest.java
+++ junit5-gradle-consumer/src/test/java/com/example/project/SecondTest.java
@@ -4,6 +4,7 @@ package com.example.project;
 import static org.junit.gen5.api.Assertions.assertEquals;
 
 import org.junit.gen5.api.Disabled;
+import org.junit.gen5.api.Tag;
 import org.junit.gen5.api.Test;
 
 class SecondTest {
@@ -14,4 +15,9 @@ class SecondTest {
 		assertEquals(2, 1, "2 is not equal to 1");
 	}
 
+	@Test
+	@Tag("slow")
+	void aSlowTest() throws InterruptedException {
+		Thread.sleep(10000);
+	}
 }
