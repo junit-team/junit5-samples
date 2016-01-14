ee496e6a72ae69269a18d55c4a0123e9a5c2a93f
Sam Brannen <sam@sambrannen.com>
2016-01-14 18:34:49 +0100

    Migrate @TestName to TestInfo

--- junit5-gradle-consumer/src/test/java/com/example/project/FirstTest.java
+++ junit5-gradle-consumer/src/test/java/com/example/project/FirstTest.java
@@ -6,17 +6,17 @@ import static org.junit.gen5.api.Assertions.assertEquals;
 import org.junit.gen5.api.DisplayName;
 import org.junit.gen5.api.Tag;
 import org.junit.gen5.api.Test;
-import org.junit.gen5.api.TestName;
+import org.junit.gen5.api.TestInfo;
 
 @Tag("fast")
 class FirstTest {
 
 	@Test
 	@DisplayName("My 1st JUnit 5 test! 😎")
-	void myFirstTest(@TestName String testName) {
+	void myFirstTest(TestInfo testInfo) {
 		Calculator calculator = new Calculator();
 		assertEquals(2, calculator.add(1, 1), "1 + 1 should equal 2");
-		assertEquals("My 1st JUnit 5 test! 😎", testName, () -> "testName is injected correctly");
+		assertEquals("My 1st JUnit 5 test! 😎", testInfo.getDisplayName(), () -> "TestInfo is injected correctly");
 	}
 
 }
