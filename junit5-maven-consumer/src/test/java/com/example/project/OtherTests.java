d0ae74037ac6ad73c93b8454b87d722b3892f3e1
Steve Moyer <smoyer@psu.edu>
2016-06-29 11:57:50 -0400

    Added support for tests classes named with the pattern *Tests.java (since that's the convention in JUnit 5).

---
+++ junit5-maven-consumer/src/test/java/com/example/project/OtherTests.java
@@ -0,0 +1,18 @@
+
+package com.example.project;
+
+import org.junit.jupiter.api.Test;
+
+public class OtherTests {
+
+	@Test
+	public void testThisThing() {
+
+	}
+
+	@Test
+	public void testThisOtherThing() {
+
+	}
+
+}
