b63069aeb7fa8b0b02a10bc9eb72d3ede554d390
Christian Stein <sormuras@gmail.com>
2018-02-23 09:43:36 +0100

    Move build statements from JShell script into Builder.java class

--- build-all-samples.jsh
+++ build-all-samples.jsh
@@ -1,26 +1,5 @@
-//usr/bin/env jshell --show-version "$0" "$@"; exit $?
+//usr/bin/env jshell --execution local --show-version "$0" "$@"; exit $?
 
 /open src/Builder.java
 
-Builder builder = new Builder()
-
-int error = 0
-void run(String sample, String exec, String... args) {
-   if (error == 0) error = builder.run(sample, exec, args);
-}
-
-run(".", "java", "--version")
-
-error = builder.checkLicense()
-
-run("junit5-vanilla-gradle", "gradlew", "clean", "test", "--no-daemon")
-run("junit5-vanilla-maven", "mvnw", "clean", "test")
-run("junit5-vanilla-modules", "jshell", "build.jsh")
-run("junit5-gradle-consumer", "gradlew", "clean", "test", "--no-daemon")
-run("junit5-maven-consumer", "mvnw", "clean", "test")
-run("junit5-mockito-extension", "gradlew", "clean", "test", "--no-daemon")
-run("junit5-java9-engine", "gradlew", "clean", "test", "--no-daemon")
-
-System.out.printf("%n%n%n|%n| Done. Build exited with %d.%n|%n", error)
-
-/exit error
+/exit new Builder().build()
--- src/Builder.java
+++ src/Builder.java
@@ -20,10 +20,37 @@ import java.util.stream.Collectors;
 /**
  * Platform-agnostic builder used by {@code build-all-samples.jsh}.
  */
-@SuppressWarnings("unused")
+@SuppressWarnings({ "WeakerAccess", "SameParameterValue" })
 class Builder {
 
-	int run(String directory, String executable, String... args) {
+	public static void main(String[] args) {
+		int status = new Builder().build();
+		if (status != 0) {
+			throw new AssertionError("Expected exit status of zero, but got: " + status);
+		}
+	}
+
+	int status = 0;
+
+	int build() {
+		System.out.printf("|%n| Building all samples...%n|%n");
+		run(".", "java", "--version");
+		checkLicense("src/eclipse-public-license-2.0.java", ".java");
+		run("junit5-vanilla-gradle", "gradlew", "clean", "test");
+		run("junit5-vanilla-maven", "mvnw", "clean", "test");
+		run("junit5-vanilla-modules", "jshell", "build.jsh");
+		run("junit5-gradle-consumer", "gradlew", "clean", "test");
+		run("junit5-maven-consumer", "mvnw", "clean", "test");
+		run("junit5-mockito-extension", "gradlew", "clean", "test");
+		run("junit5-java9-engine", "gradlew", "clean", "test");
+		System.out.printf("%n%n%n|%n| Done. Build exits with status = %d.%n|%n", status);
+		return status;
+	}
+
+	void run(String directory, String executable, String... args) {
+		if (status != 0) {
+			return;
+		}
 		System.out.printf("%n%n%n|%n| %s%n|%n", directory);
 		System.out.printf("| %s %s%n|%n", executable, String.join(" ", args));
 		Path path = Paths.get(directory);
@@ -44,43 +71,47 @@ class Builder {
 		try {
 			Process process = processBuilder.start();
 			process.getInputStream().transferTo(System.out);
-			return process.waitFor();
-		} catch (Throwable throwable) {
+			status = process.waitFor();
+		} catch (Exception exception) {
 			System.err.printf("%n%n%n| %s failed to build!%n", directory);
-			throwable.printStackTrace(System.err);
-			return 1;
+			exception.printStackTrace(System.err);
+			status = 1;
 		}
 	}
 
-	int checkLicense() throws Exception {
-		List<String> expected = Files.readAllLines(Paths.get("src/eclipse-public-license-2.0.java"));
-		expected.forEach(System.out::println);
-		int error = 0;
-		int count = 0;
-		List<Path> paths = Files.walk(Paths.get("."))
-				.filter(path -> path.getFileName().toString().endsWith(".java"))
-				.collect(Collectors.toList());
-		for (Path path : paths) {
-			int current = checkLicense(path, expected);
-			if (current != 0) {
-				System.err.printf("| %s%n", path);
-				count++;
+	void checkLicense(String blueprint, String extension) {
+		if (status != 0) {
+			return;
+		}
+		try {
+			List<String> expected = Files.readAllLines(Paths.get(blueprint));
+			expected.forEach(System.out::println);
+			int count = 0;
+			List<Path> paths = Files.walk(Paths.get(".")).filter(path -> path.getFileName().toString().endsWith(extension)).collect(Collectors.toList());
+			for (Path path : paths) {
+				if (!checkLicense(path, expected)) {
+					System.err.printf("| %s%n", path);
+					count++;
+				}
 			}
-			error += current;
+			if (count > 0) {
+				System.err.printf("| %d file(s) with no or false license.%n", count);
+				status = 1;
+			}
 		}
-		if (count > 0) {
-			System.err.printf("| %d file(s) with no or false license.%n", count);
+		catch (Exception exception) {
+			System.err.printf("%n%n%n| License `%s` check failed!%n", blueprint);
+			exception.printStackTrace(System.err);
+			status = 1;
 		}
-		return error;
 	}
 
-
-	private int checkLicense(Path path, List<String> expected) throws Exception {
+	boolean checkLicense(Path path, List<String> expected) throws Exception {
 		List<String> actual = Files.readAllLines(path);
 		if (actual.size() >= expected.size()) {
 			actual = actual.subList(0, expected.size());
-			return actual.equals(expected) ? 0 : 1;
+			return actual.equals(expected);
 		}
-		return 1;
+		return false;
 	}
 }
