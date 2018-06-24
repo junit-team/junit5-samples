ab2a4e22e6e93089bfb450f844855d39d5d1ca02
Marc Philipp <mail@marcphilipp.de>
2018-06-24 22:12:29 +0200

    Use 5.3 M1

--- junit5-jupiter-starter-ant/build.sh
+++ junit5-jupiter-starter-ant/build.sh
@@ -3,7 +3,7 @@
 #
 # Set constants.
 #
-junit_platform_version='1.2.0'
+junit_platform_version='1.3.0-M1'
 ant_version='1.10.3'
 ant_folder="apache-ant-${ant_version}"
 ant_archive="${ant_folder}-bin.tar.gz"
