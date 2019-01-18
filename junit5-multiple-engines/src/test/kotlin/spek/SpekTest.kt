b27b75b974efa9ec7270859d1cd9ffddb3d2716a
Christian Stein <sormuras@gmail.com>
2019-01-18 09:48:53 +0100

    Migrate from Spek 1 to Spek 2.x API

--- junit5-multiple-engines/src/test/kotlin/spek/SpekTest.kt
+++ junit5-multiple-engines/src/test/kotlin/spek/SpekTest.kt
@@ -10,9 +10,8 @@
 
 package spek
 
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 class SpekTest : Spek({
     describe("a simple test") {
