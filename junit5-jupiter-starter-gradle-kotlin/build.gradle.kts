3ea810e3ead96422cea57d71433382a5adff14f2
@lilo$oft <fellahi.ali@gmail.com>
2019-01-26 22:57:35 +0100

    Adds kotlin-stdlib-jdk8 compile dependency

--- junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
+++ junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
@@ -7,6 +7,8 @@ repositories {
 }
 
 dependencies {
+	implementation(kotlin("stdlib-jdk8"))
+	// or compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testCompile("org.junit.jupiter:junit-jupiter-api:5.3.2")
 	testCompile("org.junit.jupiter:junit-jupiter-params:5.3.2")
 	testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.2")
