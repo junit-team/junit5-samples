6786f20d78607d8b3480e11e18934dc3ec469ad2
Christian Stein <sormuras@gmail.com>
2019-09-06 10:37:42 +0200

    Use curl instead of wget

--- junit5-jupiter-starter-ant/build.sh
+++ junit5-jupiter-starter-ant/build.sh
@@ -11,13 +11,13 @@ ant_archive="${ant_folder}-bin.tar.gz"
 #
 # Load and extract Apache Ant.
 #
-wget --timestamping --continue "https://archive.apache.org/dist/ant/binaries/${ant_archive}"
+curl --remote-name "https://archive.apache.org/dist/ant/binaries/${ant_archive}"
 tar --extract -z --exclude "${ant_folder}/manual" --file "${ant_archive}"
 
 #
-# Load and store junit-platform-console-standalone jar in ${ANT_HOME}/lib.
+# Load and store junit-platform-console-standalone jar into ${ANT_HOME}/lib.
 #
-wget --timestamping --continue --directory-prefix "${ant_folder}/lib" "http://central.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/${junit_platform_version}/junit-platform-console-standalone-${junit_platform_version}.jar"
+(cd "${ant_folder}/lib" && curl --remote-name  "http://central.maven.org/maven2/org/junit/platform/junit-platform-console-standalone/${junit_platform_version}/junit-platform-console-standalone-${junit_platform_version}.jar")
 
 #
 # Finally, let Ant do its work...
