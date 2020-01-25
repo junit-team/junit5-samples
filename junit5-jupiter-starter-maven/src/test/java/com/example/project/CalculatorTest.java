dbc0fd8d46cd1c481f10dfb727c13cf2c2887789
dsying <dingduoduo0916@gmail.com>
2020-01-25 19:05:13 +0800

    jvm远程调试

---
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        System.out.println("hello");
+    }
+}
