06c3780c2dffb56dfaa9e11a3bbb7fddc48469db
Hao Wang <dev@flexport.com>
2020-07-27 14:57:36 +0800

    support env.XML_OUTPUT_FILE from JUnit4

--- junit5-jupiter-starter-bazel/bazeljunit5/src/main/java/com/flexport/bazeljunit5/BazelJUnit5ConsoleLauncher.java
+++ junit5-jupiter-starter-bazel/bazeljunit5/src/main/java/com/flexport/bazeljunit5/BazelJUnit5ConsoleLauncher.java
@@ -10,11 +10,16 @@
 
 package com.flexport.bazeljunit5;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import org.junit.platform.commons.util.ReflectionUtils;
 import org.junit.platform.console.ConsoleLauncher;
 
@@ -25,33 +30,95 @@ public class BazelJUnit5ConsoleLauncher {
   private static final String SELECT_CLASS = "--select-class";
   private static final String SELECT_METHOD = "--select-method";
 
+  // LegacyXmlReportGeneratingListener with junit-jupiter generates this file in `--reports-dir` by
+  // default
+  // https://github.com/junit-team/junit5/blob/37e0f559277f0065f8057cc465a1e8eb91563af6/junit-platform-reporting/src/main/java/org/junit/platform/reporting/legacy/xml/LegacyXmlReportGeneratingListener.java#L116
+  private static final String XML_OUTPUT_FILE_PATTERN = "^TEST-.*\\.xml$";
+
   /** Transform args and invoke the real implementation. */
   public static void main(String... args) {
     int exitCode =
         ConsoleLauncher.execute(System.out, System.err, transformArgs(args)).getExitCode();
+    afterExecute(exitCode);
+
     System.exit(exitCode);
+  }
+
+  /** Move the generated reports to where they should be. */
+  public static void afterExecute(int exitCode) {
+    fixXmlOutputFile(System.getenv("XML_OUTPUT_FILE"));
+  }
+
+  private static void fixXmlOutputFile(String xmlOutputFile) {
+    if (xmlOutputFile == null || xmlOutputFile.isEmpty()) {
+      return;
+    }
+
+    Path requiredPath = Paths.get(xmlOutputFile);
+    Path dir = requiredPath.getParent();
+
+    File[] files =
+        dir.toFile().listFiles((file, filename) -> filename.matches(XML_OUTPUT_FILE_PATTERN));
+    if (files == null || files.length == 0) {
+      System.err.println("The XML output file is not found");
+      return;
+    }
+
+    if (files.length > 1) {
+      // The XML output file is not found.
+      System.err.println("More than one XML output file is found");
+    }
+
+    try {
+      Files.move(files[0].toPath(), requiredPath);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   /** Transform args into JUnit5 fashion. */
   public static String[] transformArgs(String[] args) {
-    return transformArgs(args, System.getenv("TESTBRIDGE_TEST_ONLY"));
+    return transformArgsForXmlOutputFile(
+        transformArgsForTestBridgeTestOnly(
+            Arrays.asList(args), System.getenv("TESTBRIDGE_TEST_ONLY")),
+        System.getenv("XML_OUTPUT_FILE"))
+        .stream()
+        .toArray(String[]::new);
   }
 
-  private static String[] transformArgs(String[] args, String testOnly) {
-    // When e.g. `bazel test --test_filter=foo //bar:test` is run, the `--test_filter=` value is set
-    // in the TESTBRIDGE_TEST_ONLY environment variable by Bazel. For that example, value of the
-    // environment variable would simply be `foo`. Here are some examples of what it might look like
-    // in practice:
-    // test.package.TestClass#testMethod, test.package.TestClass, test.package
-    if (testOnly == null || testOnly.isEmpty()) {
+  private static List<String> transformArgsForTestBridgeTestOnly(
+      List<String> args, String testBridgeTestOnly) {
+
+    if (testBridgeTestOnly == null || testBridgeTestOnly.isEmpty()) {
       return args;
     }
 
-    return Stream.concat(
-            // remove any --select-package
-            filterOptions(Arrays.asList(args), Arrays.asList(SELECT_PACKAGE)).stream(),
-            parseOptions(testOnly).stream())
-        .toArray(String[]::new);
+    // When e.g. `bazel test --test_filter=foo //bar:test` is run, the `--test_filter=` value is
+    // set in the TESTBRIDGE_TEST_ONLY environment variable by Bazel. For that example, value of the
+    // environment variable would simply be `foo`. Here are some examples of what it might look
+    // like in practice: test.package.TestClass#testMethod, test.package.TestClass, test.package
+
+    List<String> newArgs = new ArrayList<>();
+    newArgs.addAll(filterOptions(args, Arrays.asList(SELECT_PACKAGE)));
+    newArgs.addAll(parseOptions(testBridgeTestOnly));
+
+    return newArgs;
+  }
+
+  private static List<String> transformArgsForXmlOutputFile(
+      List<String> args, String xmlOutputFile) {
+
+    if (xmlOutputFile == null || xmlOutputFile.isEmpty()) {
+      return args;
+    }
+
+    // The XML output reporting file is set in the XML_OUTPUT_FILE environment variable by JUnit4.
+    // And JUnit5 will get the reports directory from `--reports-dir`.
+
+    List<String> newArgs = new ArrayList<>(args);
+    newArgs.add("--reports-dir=" + Paths.get(xmlOutputFile).getParent().toString());
+
+    return newArgs;
   }
 
   /**
@@ -63,25 +130,25 @@ public class BazelJUnit5ConsoleLauncher {
    *
    * <p>test.package.TestClass#testMethod: --select-method=test.package.TestClass#testMethod
    *
-   * @param testOnly env.TESTBRIDGE_TEST_ONLY
+   * @param testBridgeTestOnly env.TESTBRIDGE_TEST_ONLY
    * @return option
    */
-  private static List<String> parseOptions(String testOnly) {
+  private static List<String> parseOptions(String testBridgeTestOnly) {
     // transform env.TESTBRIDGE_TEST_ONLY
 
     // test.package.TestClass#
-    if (testOnly.endsWith("#") || testOnly.endsWith("$")) {
-      testOnly = testOnly.substring(0, testOnly.length() - 1);
+    if (testBridgeTestOnly.endsWith("#") || testBridgeTestOnly.endsWith("$")) {
+      testBridgeTestOnly = testBridgeTestOnly.substring(0, testBridgeTestOnly.length() - 1);
     }
 
-    if (testOnly.contains("#")) {
-      String[] splits = testOnly.split("#");
+    if (testBridgeTestOnly.contains("#")) {
+      String[] splits = testBridgeTestOnly.split("#");
       String className = splits[0];
       String methodName = splits[1];
 
       // already in format test.package.TestClass#testMethod(...)
       if (methodName.matches(".*\\(.*\\)")) {
-        return Arrays.asList(SELECT_METHOD + "=" + testOnly);
+        return Arrays.asList(SELECT_METHOD + "=" + testBridgeTestOnly);
       }
 
       Class<?> klass;
@@ -101,11 +168,11 @@ public class BazelJUnit5ConsoleLauncher {
     }
 
     try {
-      Class.forName(testOnly);
-      return Arrays.asList(SELECT_CLASS + "=" + testOnly);
+      Class.forName(testBridgeTestOnly);
+      return Arrays.asList(SELECT_CLASS + "=" + testBridgeTestOnly);
     } catch (ClassNotFoundException e) {
       // should be a package
-      return Arrays.asList(SELECT_PACKAGE + "=" + testOnly);
+      return Arrays.asList(SELECT_PACKAGE + "=" + testBridgeTestOnly);
     }
   }
 
--- junit5-jupiter-starter-bazel/bazeljunit5/src/test/java/com/flexport/bazeljunit5/BazelJUnit5ConsoleLauncherTest.java
+++ junit5-jupiter-starter-bazel/bazeljunit5/src/test/java/com/flexport/bazeljunit5/BazelJUnit5ConsoleLauncherTest.java
@@ -13,7 +13,13 @@ package com.flexport.bazeljunit5;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,24 +41,91 @@ class BazelJUnit5ConsoleLauncherTest {
     void testMethod1() {}
   }
 
+  @ParameterizedTest
+  @CsvSource(
+      value = {
+          ",false,",
+          "foo.xml,false,",
+          "TEST-foo.xml,false,TEST-foo.xml",
+          "foo.xml|TEST-bar.xml|TEST-baz.xml,false,TEST-bar.xml|TEST-baz.xml",
+          "foo.xml|TEST-bar.xml|TEST-baz.xml,true,",
+      })
+  void fixXmlOutputFile(String files, boolean isXmlOutputFileEmpty, String expectedContents)
+      throws IOException {
+    Path tempFile = File.createTempFile("dummy-", ".dummy").toPath();
+    Path reportsDir = Paths.get(tempFile.toString() + ".dir");
+    Files.createDirectory(reportsDir);
+
+    if (files != null && !files.isEmpty()) {
+      String[] splits = files.split("\\|");
+      for (String split : splits) {
+        Files.write(reportsDir.resolve(split), split.getBytes());
+      }
+    }
+    Path xmlOutputFile = reportsDir.resolve("test.xml");
+
+    ReflectionTestUtils.invokeMethod(
+        BazelJUnit5ConsoleLauncher.class,
+        "fixXmlOutputFile",
+        isXmlOutputFileEmpty ? "" : xmlOutputFile.toString());
+
+    if (expectedContents != null && !expectedContents.isEmpty()) {
+      assertThat(xmlOutputFile.toFile().exists()).isTrue();
+
+      String content = new String(Files.readAllBytes(xmlOutputFile), StandardCharsets.UTF_8);
+      assertThat(expectedContents.split("\\|")).contains(content);
+    } else {
+      assertThat(xmlOutputFile).doesNotExist();
+    }
+  }
+
   @ParameterizedTest(name = "{0}; {1} -> {2}")
   @CsvSource(
       value = {
-        "''; ; ''",
-        "--opt Opt; ; --opt Opt",
-        "--opt Opt; ''; --opt Opt",
-        "''; com.flexport.bazeljunit5; --select-package=com.flexport.bazeljunit5",
+          "''; ; ''",
+          "--opt Opt; ; --opt Opt",
+          "--opt Opt; ''; --opt Opt",
+          "''; com.flexport.bazeljunit5; --select-package=com.flexport.bazeljunit5",
       },
       delimiter = ';')
-  void transformArgs(String argsText, String testOnly, String expectedArgsText) {
-    String[] args = argsText.isEmpty() ? new String[] {} : argsText.split(" ");
+  void transformArgsForTestBridgeTestOnly(
+      String argsText, String testBridgeTestOnly, String expectedArgsText) {
+    List<String> args = argsText.isEmpty() ? new ArrayList<>() : Arrays.asList(argsText.split(" "));
 
-    String[] newArgs =
+    List<String> newArgs =
         ReflectionTestUtils.invokeMethod(
-            BazelJUnit5ConsoleLauncher.class, "transformArgs", args, testOnly);
+            BazelJUnit5ConsoleLauncher.class,
+            "transformArgsForTestBridgeTestOnly",
+            args,
+            testBridgeTestOnly);
 
-    String[] expectedArgs =
-        expectedArgsText.isEmpty() ? new String[] {} : expectedArgsText.split(" ");
+    List<String> expectedArgs =
+        expectedArgsText.isEmpty() ? new ArrayList<>() : Arrays.asList(expectedArgsText.split(" "));
+
+    assertThat(newArgs).isEqualTo(expectedArgs);
+  }
+
+  @ParameterizedTest(name = "{0}; {1} -> {2}")
+  @CsvSource(
+      value = {
+          "''; ; ''",
+          "''; ''; ''",
+          "--opt Opt; ; --opt Opt",
+          "--opt Opt; ''; --opt Opt",
+          "''; foo/bar.xml; --reports-dir=foo",
+          "--opt Opt; foo/bar.xml; --opt Opt --reports-dir=foo",
+      },
+      delimiter = ';')
+  void transformArgsForXmlOutputFile(
+      String argsText, String xmlOutputFile, String expectedArgsText) {
+    List<String> args = argsText.isEmpty() ? new ArrayList<>() : Arrays.asList(argsText.split(" "));
+
+    List<String> newArgs =
+        ReflectionTestUtils.invokeMethod(
+            BazelJUnit5ConsoleLauncher.class, "transformArgsForXmlOutputFile", args, xmlOutputFile);
+
+    List<String> expectedArgs =
+        expectedArgsText.isEmpty() ? new ArrayList<>() : Arrays.asList(expectedArgsText.split(" "));
 
     assertThat(newArgs).isEqualTo(expectedArgs);
   }
@@ -60,34 +133,34 @@ class BazelJUnit5ConsoleLauncherTest {
   @ParameterizedTest(name = "{0} -> {1}")
   @CsvSource(
       value = {
-        "{package}; --select-package={package}",
-        "{class}; --select-class={class}",
-        "{class}#; --select-class={class}",
-        "{class}#testMethod; "
-            + "--select-method={class}#testMethod()|"
-            + "--select-method={class}#testMethod(int)|"
-            + "--select-method={class}#testMethod(int, java.lang.Integer)",
-        "{class}#testMethod$; "
-            + "--select-method={class}#testMethod()|"
-            + "--select-method={class}#testMethod(int)|"
-            + "--select-method={class}#testMethod(int, java.lang.Integer)",
-        "{class}#testMethod(); --select-method={class}#testMethod()",
-        "{class}#testMethod1; --select-method={class}#testMethod1()",
+          "{package}; --select-package={package}",
+          "{class}; --select-class={class}",
+          "{class}#; --select-class={class}",
+          "{class}#testMethod; "
+              + "--select-method={class}#testMethod()|"
+              + "--select-method={class}#testMethod(int)|"
+              + "--select-method={class}#testMethod(int, java.lang.Integer)",
+          "{class}#testMethod$; "
+              + "--select-method={class}#testMethod()|"
+              + "--select-method={class}#testMethod(int)|"
+              + "--select-method={class}#testMethod(int, java.lang.Integer)",
+          "{class}#testMethod(); --select-method={class}#testMethod()",
+          "{class}#testMethod1; --select-method={class}#testMethod1()",
       },
       delimiter = ';')
-  void parseOptions(String testOnly, String expectedOptionsText) {
+  void parseOptions(String testBridgeTestOnly, String expectedOptionsText) {
     List<String> options =
         ReflectionTestUtils.invokeMethod(
             BazelJUnit5ConsoleLauncher.class,
             "parseOptions",
-            testOnly.replace("{class}", TestClass.class.getName()));
+            testBridgeTestOnly.replace("{class}", TestClass.class.getName()));
 
     List<String> expectedOptions =
         expectedOptionsText.isEmpty()
             ? Arrays.asList()
             : Arrays.stream(expectedOptionsText.split("\\|"))
-                .map(s -> s.replace("{class}", TestClass.class.getName()))
-                .collect(Collectors.toList());
+            .map(s -> s.replace("{class}", TestClass.class.getName()))
+            .collect(Collectors.toList());
 
     assertThat(options).hasSameElementsAs(expectedOptions);
   }
@@ -110,14 +183,14 @@ class BazelJUnit5ConsoleLauncherTest {
   @ParameterizedTest(name = "{0} -> {1}")
   @CsvSource(
       value = {
-        "'', ''",
-        "--select-package com.flexport, ''",
-        "--select-package=com.flexport, ''",
-        "--select-package, ''",
-        "--select-package=com.flexport --select-package com.flexport --opt=Opt, --opt=Opt",
-        "--opt=Opt, --opt=Opt",
-        "--opt Opt, --opt Opt",
-        "Opt, Opt",
+          "'', ''",
+          "--select-package com.flexport, ''",
+          "--select-package=com.flexport, ''",
+          "--select-package, ''",
+          "--select-package=com.flexport --select-package com.flexport --opt=Opt, --opt=Opt",
+          "--opt=Opt, --opt=Opt",
+          "--opt Opt, --opt Opt",
+          "Opt, Opt",
       })
   void filterOptions(String argsText, String expectedArgsText) {
     List<String> args = argsText.isEmpty() ? Arrays.asList() : Arrays.asList(argsText.split(" "));
