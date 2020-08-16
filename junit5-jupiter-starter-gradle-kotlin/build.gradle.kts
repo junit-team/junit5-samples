5feb07f216c715d9102da9e7158efaaa1e58ab5b
Marc Philipp <mail@marcphilipp.de>
2020-08-16 18:33:40 +0200

    Use 5.7.0-RC1

--- junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
+++ junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib-jdk8"))
-	testImplementation(platform("org.junit:junit-bom:5.6.2"))
+	testImplementation(platform("org.junit:junit-bom:5.7.0-RC1"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
