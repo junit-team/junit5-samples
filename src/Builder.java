b856dc2399adc80dec2b57acab5c349e978df0cc
Marc Philipp <mail@marcphilipp.de>
2021-03-19 09:58:07 +0100

    Show Bazel test output

--- src/Builder.java
+++ src/Builder.java
@@ -44,7 +44,7 @@ class Builder {
 		run("junit5-jupiter-starter-gradle-kotlin", "gradlew", "clean", "test");
 		run("junit5-jupiter-starter-maven", "mvnw", "--batch-mode", "clean", "test");
 		run("junit5-jupiter-starter-maven-kotlin", "mvnw", "--batch-mode", "clean", "test");
-		run("junit5-jupiter-starter-bazel", "bazelisk.py", "test", "//...");
+		run("junit5-jupiter-starter-bazel", "bazelisk.py", "test", "//...", "--test_output", "all");
 
 		// jupiter-extensions
 		run("junit5-jupiter-extensions", "gradlew", "clean", "test");
