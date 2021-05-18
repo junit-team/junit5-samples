c50c2bd0e333609e4a3caa9ca6813817469d7927
Marc Philipp <mail@marcphilipp.de>
2021-05-18 21:08:28 +0200

    Rename for consistency

--- junit5-multiple-engines/src/test/java/junit/jupiter/JupiterTests.java
+++ junit5-multiple-engines/src/test/java/junit/jupiter/JupiterTests.java
@@ -12,11 +12,14 @@ package junit.jupiter;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class JUnit5Tests {
+class JupiterTests {
 
 	@Test
+	@DisplayName("1 + 2 = 3")
 	void test() {
 		assertEquals(3, 1 + 2);
 	}
