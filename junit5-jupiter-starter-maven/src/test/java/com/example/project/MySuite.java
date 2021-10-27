959b3769435083bca76ecc39fa44cc6493107e5f
Marc Philipp <mail@marcphilipp.de>
2021-10-27 08:33:10 +0200

    Configure junit-platform-suite engine

---
+++ junit5-jupiter-starter-maven/src/test/java/com/example/project/MySuite.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2015-2021 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example.project;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("com.example.project")
+public class MySuite {
+}
