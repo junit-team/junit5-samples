5f9e577e2c64b06f8b7ef5633462144feed97fef
Marc Philipp <mail@marcphilipp.de>
2021-11-28 16:41:43 +0100

    Use 5.8.2

--- junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
+++ junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-	testImplementation(platform("org.junit:junit-bom:5.8.1"))
+	testImplementation(platform("org.junit:junit-bom:5.8.2"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
--- junit5-multiple-engines/build.gradle.kts
+++ junit5-multiple-engines/build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     val junit4Version = "4.13.2"
-    val junitBomVersion = "5.8.1"
+    val junitBomVersion = "5.8.2"
 
     // Use junit-bom to align versions
     // https://docs.gradle.org/current/userguide/managing_transitive_dependencies.html#sec:bom_import
