d6a30a874105d98b10cad7c50a920cd6f6639030
Marc Philipp <mail@marcphilipp.de>
2023-11-05 17:51:57 +0100

    Use 5.10.1

--- junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
+++ junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-	testImplementation(platform("org.junit:junit-bom:5.10.0"))
+	testImplementation(platform("org.junit:junit-bom:5.10.1"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
