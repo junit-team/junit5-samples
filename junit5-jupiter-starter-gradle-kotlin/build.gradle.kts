3bef8071b75f6a10ad12460ffa47cbdf969e77c2
Juan Moreno <7319391+JuanMorenoDeveloper@users.noreply.github.com>
2024-01-02 20:52:38 -0300

    Update build.gradle

--- junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
+++ junit5-jupiter-starter-gradle-kotlin/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 dependencies {
 	testImplementation(platform("org.junit:junit-bom:5.10.1"))
 	testImplementation("org.junit.jupiter:junit-jupiter")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
