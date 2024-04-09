08f49839a318233e18762de97e4c39604bd4d6a8
Christian Stein <sormuras@gmail.com>
2024-04-09 09:18:21 +0200

    Update TestNG to 7.10.1

--- junit5-multiple-engines/build.gradle.kts
+++ junit5-multiple-engines/build.gradle.kts
@@ -72,7 +72,7 @@ dependencies {
     }
 
     // TestNG
-    testImplementation("org.testng:testng:7.9.0") {
+    testImplementation("org.testng:testng:7.10.1") {
         because("allows writing TestNG tests")
     }
     testRuntimeOnly("org.junit.support:testng-engine:1.0.5") {
