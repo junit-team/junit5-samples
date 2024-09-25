39b2c03dd7af07fcfa5e6b562795c78289a69ec2
Marc Philipp <mail@marcphilipp.de>
2024-09-25 09:04:20 +0200

    Use 5.11.1

--- junit5-multiple-engines/build.gradle.kts
+++ junit5-multiple-engines/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 
 dependencies {
     val junit4Version = "4.13.2"
-    val junitBomVersion = "5.11.0"
+    val junitBomVersion = "5.11.1"
 
     // Use junit-bom to align versions
     // https://docs.gradle.org/current/userguide/managing_transitive_dependencies.html#sec:bom_import
