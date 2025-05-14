7b2601d8b8ac266bf86c7acd0fd2301ef0a8378b
Marc Philipp <mail@marcphilipp.de>
2025-05-14 19:32:29 +0200

    Update injector to work with Maven Central Publisher Portal endpoint (#553)
    see junit-team/junit5#4531

--- src/StagingRepoInjector.java
+++ src/StagingRepoInjector.java
@@ -18,62 +18,70 @@ import java.util.function.Consumer;
 public class StagingRepoInjector {
 
     public static void main(String[] args) throws Exception {
-        new StagingRepoInjector(args[0]).appendAfter();
+        new StagingRepoInjector(args[0]).inject();
     }
 
     private final String stagingRepoUrl;
 
-    public StagingRepoInjector(String stagingRepoUrl) {
+    private StagingRepoInjector(String stagingRepoUrl) {
         this.stagingRepoUrl = stagingRepoUrl;
     }
 
-    private void appendAfter() throws Exception {
+    private void inject() throws Exception {
+
+        var gradleGroovyDslSnippet = """
+                
+                maven {
+                    url = '%s'
+                    credentials(HttpHeaderCredentials) {
+                        name = 'Authorization'
+                        value = "Bearer ${System.getenv('MAVEN_CENTRAL_USER_TOKEN')}"
+                    }
+                    authentication {
+                        header(HttpHeaderAuthentication)
+                    }
+                }
+                """
+                .formatted(stagingRepoUrl);
+
+        var gradleKotlinDslSnippet = """
+                
+                maven {
+                    url = uri("%s")
+                    credentials(HttpHeaderCredentials::class) {
+                        name = "Authorization"
+                        value = "Bearer ${System.getenv("MAVEN_CENTRAL_USER_TOKEN")}"
+                    }
+                    authentication {
+                        create<HttpHeaderAuthentication>("header")
+                    }
+                }
+                """
+                .formatted(stagingRepoUrl);
 
         appendAfter("junit5-jupiter-extensions/build.gradle", "mavenCentral()",
-                "%n\tmaven { url = '%s' }".formatted(stagingRepoUrl));
+                gradleGroovyDslSnippet);
 
-        replace("junit5-jupiter-starter-ant/build.sh", "https://repo1.maven.org/maven2", stagingRepoUrl);
-
-        appendAfter("junit5-jupiter-starter-bazel/MODULE.bazel", "\"https://repo1.maven.org/maven2\",",
-                "%n        \"%s\",".formatted(stagingRepoUrl));
+        replace("junit5-jupiter-starter-ant/build.sh", "\"https://repo1.maven.org/maven2",
+                "--header \"Authorization: Bearer $MAVEN_CENTRAL_USER_TOKEN\" \"%s".formatted(stagingRepoUrl));
 
         appendAfter("junit5-jupiter-starter-gradle/build.gradle", "mavenCentral()",
-                "%n\tmaven { url = '%s' }".formatted(stagingRepoUrl));
+                gradleGroovyDslSnippet);
 
         appendAfter("junit5-jupiter-starter-gradle-groovy/build.gradle", "mavenCentral()",
-                "%n\tmaven { url = '%s' }".formatted(stagingRepoUrl));
+                gradleGroovyDslSnippet);
 
         appendAfter("junit5-jupiter-starter-gradle-kotlin/build.gradle.kts", "mavenCentral()",
-                "%n\tmaven(url = \"%s\")".formatted(stagingRepoUrl));
-
-        var mavenPomRepo = """
-                
-                
-                \t<repositories>
-                \t\t<repository>
-                \t\t\t<id>central-staging</id>
-                \t\t\t<url>%s</url>
-                \t\t</repository>
-                \t</repositories>
-                """.stripIndent().stripTrailing().formatted(stagingRepoUrl);
-
-        appendAfter("junit5-jupiter-starter-maven/pom.xml", "</build>", mavenPomRepo);
-
-        appendAfter("junit5-jupiter-starter-maven-kotlin/pom.xml", "</build>", mavenPomRepo);
-
-        appendAtEnd("junit5-jupiter-starter-sbt/build.sbt",
-                "%nresolvers += \"central-staging\" at \"%s\"%n".formatted(stagingRepoUrl));
+                gradleKotlinDslSnippet);
 
         appendAfter("junit5-migration-gradle/build.gradle", "mavenCentral()",
-                "%n\tmaven { url = '%s' }".formatted(stagingRepoUrl));
-
-        appendAfter("junit5-migration-maven/pom.xml", "</build>", mavenPomRepo);
+                gradleGroovyDslSnippet);
 
         replace("junit5-modular-world/BUILDING", "\"https://repo1.maven.org/maven2\"",
                 "group.startsWith(\"org.junit\") ? \"%s\" : \"https://repo1.maven.org/maven2\"".formatted(stagingRepoUrl));
 
         appendAfter("junit5-multiple-engines/build.gradle.kts", "mavenCentral()",
-                "%n\tmaven(url = \"%s\")".formatted(stagingRepoUrl));
+                gradleKotlinDslSnippet);
     }
 
     void appendAfter(String path, String token, String addedContent) throws IOException {
@@ -92,10 +100,6 @@ public class StagingRepoInjector {
             }
             content.replace(content.indexOf(token), content.indexOf(token) + token.length(), replacement);
         });
-    }
-
-    void appendAtEnd(String path, String addedContent) throws IOException {
-        process(path, content -> content.append(addedContent));
     }
 
     void process(String path, Consumer<StringBuilder> modification) throws IOException {
